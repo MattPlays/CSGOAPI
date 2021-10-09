70e8c0c6f97c0fa3fb00fe8b254ef3b08c6d9b6f
MattPlays <markgpmcpe@gmail.com>
2021-10-09 11:48:39 -0400

    axios rewrite

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-const fetch = require("node-fetch")
+const {create} = require("axios").default;
 const SegmentResponse = require("./SegmentResponse");
 const SearchResult = require("./SearchResult");
 const Profile = require("./Profile");
 class CSGOAPI {
     constructor(authKey) {
-        this.api = "https://public-api.tracker.gg/v2/csgo/standard/"
-        this.authKey = authKey
-        this.offical = false
+        this.instance = create({
+            baseURL: "https://public-api.tracker.gg/v2/csgo/standard",
+            method: "GET",
+            headers: {
+                "TRN-Api-Key": authKey,
+                "Accept": "application/json"
+            }
+        })
     }
     /**
      * 
      * @param {string} playerIndentifer 
      * @returns {Promise<Profile>}
      */
-    GetPlayerStats(playerIndentifer) {
-        let url = this.api + `profile/steam/${playerIndentifer}`
-        return fetch(url, {
-            "headers": {
-                "TRN-Api-Key": this.authKey,
-                "Accept": "application/json",
-            },
-            "method": "GET",
-            "mode": "cors"
-        }).then(res => res.json()).then((data) => {
-            return new Profile(data.data.platformInfo, data.data.userInfo, data.data.metadata, data.data.segments, data.data.availableSegments, data.data.expiryDate)
-        }).catch((err) => {throw new Error(err)});
+    async GetPlayerStats(playerIndentifer) {
+        const {data} = await this.instance({
+            url: `/profile/steam/${playerIndentifer}`,
+            responseType: "json"
+        });
+        return new Profile(data.platformInfo, data.userInfo, data.metadata, data.segments, data.availableSegments, data.expiryDate)
     }
     /**
      * 
@@ -32,36 +31,28 @@ class CSGOAPI {
      * @param {string} segmentType 
      * @returns {Promise<SegmentResponse[]>}
      */
-    GetStatSegement(playerIndentifer, segmentType) {
+   async GetStatSegement(playerIndentifer, segmentType) {
         let url = this.api + `profile/steam/${playerIndentifer}/segments/${segmentType}`
-        return fetch(url, {
-            "headers": {
-                "TRN-Api-Key": this.authKey,
-                "Accept": "application/json",
-            },
-            "method": "GET",
-            "mode": "cors"
-        }).then(res => res.json()).then((data) => {
-            return data.map((d) => {return new SegmentResponse(d.type, d.attributes, d.metadata, d.expiryDate, d.stats)})
-        }).catch((err) => {throw new Error(err)});
+        const {data} = await this.instance({
+            url: `profile/steam/${playerIndentifer}/segments/${segmentType}`,
+        });
+        return data.map((d) => {return new SegmentResponse(d.type, d.attributes, d.metadata, d.expiryDate, d.stats)})
     }
     /**
      * 
      * @param {string} query 
      * @returns {Promise<SearchResult[]>}
      */
-    SearchForPlayer(query = "") {
+    async SearchForPlayer(query = "") {
         let url = this.api + `search?platform=steam&query=${query}`
-        return fetch(url, {
-            "headers": {
-                "TRN-Api-Key": this.authKey,
-                "Accept": "application/json",
-            },
-            "method": "GET",
-            "mode": "cors"
-        }).then(res => res.json()).then((data) => {
-            return data.map((d) => {return new SearchResult(d.platformId, d.platformSlug, d.platformUserIdentifier, d.platformUserId, d.platformUserHandle, d.avatarUrl, d.stats, d.additionalParameters)})
-        }).catch((err) => {throw new Error(err)});
+        const {data} = await this.instance({
+            url: `/search`,
+            params: {
+                "platform": "steam",
+                "query": query
+            }
+        });
+        return data.map((d) => {return new SearchResult(d.platformId, d.platformSlug, d.platformUserIdentifier, d.platformUserId, d.platformUserHandle, d.avatarUrl, d.stats, d.additionalParameters)})
     }
 }
 module.exports = {
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,83 @@
-const Profile = require('./Profile');
-const SegmentResponse = require("./SegmentResponse");
-const SearchResult = require("./SearchResult")
-export class CSGOAPI {
+declare interface Stat {
+    rank: string,
+    percentile: number,
+    displayName: string,
+    displayCategory: string,
+    category: string,
+    metadata: [],
+    value: number,
+    displayValue: string,
+    displayType: string,
+}
+declare interface platformInfo {
+    platformSlug: string,
+    platformUserId: string,
+    platformUserHandle: string,
+    platformUserIdentifier: string,
+    avatarUrl: string,
+    additionalParameters: string[],
+}
+declare interface userInfo {
+    userId: number,
+    isPremium: boolean,
+    isVerified: boolean,
+    isInfluencer: boolean,
+    isPartner: boolean,
+    countryCode: string,
+    customAvatarUrl: string,
+    customHeroUrl: string,
+    socialAccounts: string[],
+    pageviews: number,
+    isSuspicious: boolean,
+}
+declare interface Profile {
+    platformInfo: platformInfo,
+    userInfo: userInfo,
+    metadata: object | null,
+    segments: {
+        type: string, 
+        attributes: {}, 
+        metadata: {
+            name: string
+        }, 
+        expiryDate: string, 
+        stats: Stat[];
+    }[],
+    availableSegments: any,
+    expiryDate: string,
+}
+declare interface SegmentResponse {
+    type: string,
+    attributes: {
+        key: string,
+    },
+    metadata: {
+        name: string,
+        imageUrl: string;
+        category: {
+            value: string, 
+            displayValue: string
+        },
+    },
+    expiryDate: string,
+    stats: {
+        kills: Stat,
+        shotsFired: Stat,
+        shotsHit: Stat,
+        shotsAccuracy: Stat,
+    },
+}
+declare interface SearchResult {
+    platformId: number,
+    platformSlug: string,
+    platformUserIdentifier: string,
+    platformUserId: string,
+    platformUserHandle: string,
+    avatarUrl: string,
+    status: string,
+    additionalParameters: string[],
+}
+export declare class CSGOAPI {
     constructor(authKey: string);
     GetPlayerStats(playerIdentifier: string): Promise<Profile>;
     GetStatSegment(playerIdentifier: string, segmentType: string): Promise<SegmentResponse[]>;
